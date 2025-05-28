b413408dc8e90fa019383c137c372f3fc3a972d4
Ivan Ermakov <w3rew@yandex-team.ru>
2025-05-29 01:01:44 +0300

    Add MMLU eval

---
+++ evaluate_mmlu.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python
+
+import csv
+import json
+import argparse
+import os
+import torch
+import random
+import time
+import re
+from tqdm import trange
+import logging
+import sys
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig, AutoConfig
+
+from functools import partial
+from LongBench.custom_cache import get_aqua_cache
+
+
+COT_PROMPT = 'The following are multiple choice questions (with answers) about {$}. Think step by step and then finish your answer with "the answer is (X)" where X is the correct letter choice.\n\n'
+
+choices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"]
+max_model_length = 4096
+max_new_tokens = 2048
+batch_size = 32
+random.seed(12345)
+
+def load_mmlu_pro():
+    dataset = load_dataset("TIGER-Lab/MMLU-Pro")
+    test_df, val_df = dataset["test"], dataset["validation"]
+    test_df = preprocess(test_df)
+    val_df = preprocess(val_df)
+    return test_df, val_df
+
+
+def load_model():
+    llm = AutoModelForCausalLM.from_pretrained(args.model, device_map="auto", torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2")
+    tokenizer = AutoTokenizer.from_pretrained(args.model, trust_remote_code=True, padding_side="left", dtype=torch.bfloat16)
+    tokenizer.pad_token = tokenizer.eos_token
+    config = AutoConfig.from_pretrained(args.model)
+    return llm, tokenizer, config
+
+
+def preprocess(test_df):
+    res_df = []
+    for each in test_df:
+        options = []
+        for opt in each["options"]:
+            if opt == "N/A":
+                continue
+            options.append(opt)
+        each["options"] = options
+        res_df.append(each)
+    return res_df
+
+
+def args_generate_path(input_args):
+    scoring_method = "CoT"
+    model_name = input_args.model.split("/")[-1]
+    subjects = args.selected_subjects.replace(",", "-").replace(" ", "_")
+    return [model_name, scoring_method, subjects]
+
+
+def select_by_category(df, subject):
+    res = []
+    for each in df:
+        if each["category"] == subject:
+            res.append(each)
+    return res
+
+
+def format_cot_example(example, including_answer=True):
+    prompt = "Question:\n"
+    question = example["question"]
+    options = example["options"]
+    prompt += question + "\n"
+    prompt += "Options:\n"
+    for i, opt in enumerate(options):
+        prompt += "{}. {}\n".format(choices[i], opt)
+    if including_answer:
+        cot_content = example["cot_content"].replace("A: Let's think step by step.",
+                                                     "Answer: Let's think step by step.")
+        prompt += cot_content + "\n\n"
+    else:
+        prompt += "Answer: Let's think step by step."
+    return prompt
+
+
+def generate_cot_prompt(val_df, curr, k):
+    prompt = COT_PROMPT
+    subject = curr["category"]
+    val_df = select_by_category(val_df, subject)
+    val_df = val_df[: k]
+    prompt = prompt.replace("{$}", subject) + "\n"
+    for example in val_df:
+        prompt += format_cot_example(example, including_answer=True)
+    prompt += format_cot_example(curr, including_answer=False)
+    return prompt
+
+
+def extract_answer(text):
+    pattern = r"answer is \(?([A-J])\)?"
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1)
+    else:
+        print("1st answer extract failed\n" + text)
+        return extract_again(text)
+
+
+def extract_again(text):
+    match = re.search(r'.*[aA]nswer:\s*([A-J])', text)
+    if match:
+        return match.group(1)
+    else:
+        return extract_final(text)
+
+
+def extract_final(text):
+    pattern = r"\b[A-J]\b(?!.*\b[A-J]\b)"
+    match = re.search(pattern, text, re.DOTALL)
+    if match:
+        return match.group(0)
+    else:
+        return None
+
+
+def batch_inference(model, tokenizer, inference_texts, cache):
+    tokenized = tokenizer(inference_texts, return_tensors="pt", padding=True, return_attention_mask=True)
+    inference_batch = tokenized.input_ids.cuda()
+    start = time.time()
+    generation_config = GenerationConfig(
+        max_new_tokens=max_new_tokens, stop_strings=["Question:"],
+        do_sample=False, num_beams=1
+    )
+    outputs = model.generate(inference_batch, generation_config, tokenizer=tokenizer,
+                             use_cache=True, past_key_values=cache("cuda:0"),
+                             attention_mask=tokenized.attention_mask.cuda())
+    logging.info(str(len(inference_batch)) + "size batch costing time: " + str(time.time() - start))
+    response_batch = []
+    pred_batch = []
+    outputs = tokenizer.batch_decode(outputs[:, inference_batch.size(1):], skip_special_tokens=True)
+    for output in outputs:
+        response_batch.append(output)
+        pred = extract_answer(output)
+        pred_batch.append(pred)
+    return pred_batch, response_batch
+
+
+def save_res(res, output_path):
+    accu, corr, wrong = 0.0, 0.0, 0.0
+    with open(output_path, "w") as fo:
+        fo.write(json.dumps(res))
+    for each in res:
+        if not each["pred"]:
+            x = random.randint(0, len(each["options"]) - 1)
+            if x == each["answer_index"]:
+                corr += 1
+                # print("random hit.")
+            else:
+                wrong += 1
+        elif each["pred"] == each["answer"]:
+            corr += 1
+        else:
+            wrong += 1
+    if corr + wrong == 0:
+        return 0.0, 0.0, 0.0
+    accu = corr / (corr + wrong)
+    return accu, corr, wrong
+
+
+@torch.no_grad()
+def eval_cot(subject, model, tokenizer, val_df, test_df, output_path, cache):
+    global choices
+    logging.info("evaluating " + subject)
+    inference_data = []
+    pred = []
+    response = []
+
+    for i in range(len(test_df)):
+        k = args.ntrain
+        curr = test_df[i]
+        prompt = generate_cot_prompt(val_df, curr, k)
+        inference_data.append(prompt)
+
+    for i in trange(0, len(inference_data), batch_size):
+        inference_batch = inference_data[i:i + batch_size]
+        pred_batch, response_batch = batch_inference(model, tokenizer, inference_batch, cache)
+        pred += pred_batch
+        response += response_batch
+
+        torch.cuda.empty_cache()
+    res = []
+    for j, curr in enumerate(test_df):
+        curr["pred"] = pred[j]
+        curr["model_outputs"] = response[j]
+        res.append(curr)
+    accu, corr, wrong = save_res(res, output_path)
+    logging.info("this batch accu is: {}, corr: {}, wrong: {}\n".format(str(accu), str(corr), str(wrong)))
+
+    accu, corr, wrong = save_res(res, output_path)
+    return accu, corr, wrong
+
+
+def main():
+    model, tokenizer, config = load_model()
+
+    key_values = torch.load(args.predictors, weights_only=False)
+    key_predictors, value_predictors = key_values["key_predictors"], key_values["value_predictors"]    
+    cache = partial(get_aqua_cache,
+        hadamard_groupsize=args.hadamard_groupsize,
+        edenn_n=args.edenn_n,
+        edenn_d=args.edenn_d,
+        recent_buffer_size=args.recent_buffer_size,
+        prefix_size=args.prefix_size,
+        config=config,
+        key_predictors=key_predictors,
+        value_predictors=value_predictors,
+        quantizer_type="higgs",
+        not_quantize_first_layer=False
+    )
+
+
+    
+    if not os.path.exists(save_result_dir):
+        os.makedirs(save_result_dir)
+
+    full_test_df, full_val_df = load_mmlu_pro()
+    all_subjects = []
+    for each in full_test_df:
+        if each["category"] not in all_subjects:
+            all_subjects.append(each["category"])
+    if args.selected_subjects == "all":
+        selected_subjects = all_subjects
+    else:
+        selected_subjects = []
+        args_selected = args.selected_subjects.split(",")
+        for sub in all_subjects:
+            for each in args_selected:
+                if each.replace(" ", "_") in sub.replace(" ", "_"):
+                    selected_subjects.append(sub)
+    logging.info("selected subjects:\n" + "\n".join(selected_subjects))
+    print("selected subjects:\n" + "\n".join(selected_subjects))
+    sta_dict = {}
+    selected_subjects = sorted(selected_subjects)
+    with open(os.path.join(summary_path), 'a') as f:
+        f.write("\n------category level sta------\n")
+    for subject in selected_subjects:
+        if subject not in sta_dict:
+            sta_dict[subject] = {"corr": 0.0, "wrong": 0.0, "accu": 0.0}
+        test_df = select_by_category(full_test_df, subject)
+        val_df = select_by_category(full_val_df, subject)
+        output_path = os.path.join(save_result_dir, "{}.json".format(subject))
+        acc, corr_count, wrong_count = eval_cot(subject, model, tokenizer, val_df, test_df, output_path, cache)
+        sta_dict[subject]["corr"] = corr_count
+        sta_dict[subject]["wrong"] = wrong_count
+        sta_dict[subject]["accu"] = acc
+        with open(os.path.join(summary_path), 'a') as f:
+            f.write("Average accuracy {:.4f} - {}\n".format(sta_dict[subject]["accu"], subject))
+    total_corr, total_wrong = 0.0, 0.0
+    for k, v in sta_dict.items():
+        total_corr += v["corr"]
+        total_wrong += v["wrong"]
+    total_accu = total_corr / (total_corr + total_wrong + 0.000001)
+    sta_dict["total"] = {"corr": total_corr, "wrong": total_wrong, "accu": total_accu}
+
+    with open(os.path.join(summary_path), 'a') as f:
+        f.write("\n------average acc sta------\n")
+        weighted_acc = total_accu
+        f.write("Average accuracy: {:.4f}\n".format(weighted_acc))
+    with open(global_record_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+        record = args_generate_path(args) + [time_str, weighted_acc]
+        writer.writerow(record)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ntrain", "-k", type=int, default=5)
+    parser.add_argument("--selected_subjects", "-sub", type=str, default="all")
+    parser.add_argument("--save_dir", "-s", type=str, default="results")
+    parser.add_argument("--global_record_file", "-grf", type=str,
+                        default="eval_record_collection.csv")
+    parser.add_argument("--model", "-m", type=str)
+
+    parser.add_argument("--hadamard_groupsize", type=int)
+    parser.add_argument("--edenn_n", type=int)
+    parser.add_argument("--edenn_d", type=int)
+    parser.add_argument("--recent_buffer_size", type=int)
+    parser.add_argument("--prefix_size", type=int, default=4)
+    parser.add_argument("--predictors", type=str)
+
+    args = parser.parse_args()
+
+    os.makedirs(args.save_dir, exist_ok=True)
+    global_record_file = args.global_record_file
+    save_result_dir = os.path.join(
+        args.save_dir, "/".join(args_generate_path(args))
+    )
+    file_prefix = "-".join(args_generate_path(args))
+    timestamp = time.time()
+    time_str = time.strftime('%m-%d_%H-%M', time.localtime(timestamp))
+    file_name = f"{file_prefix}_{time_str}_summary.txt"
+    summary_path = os.path.join(args.save_dir, "summary", file_name)
+    os.makedirs(os.path.join(args.save_dir, "summary"), exist_ok=True)
+    os.makedirs(save_result_dir, exist_ok=True)
+    save_log_dir = os.path.join(args.save_dir, "log")
+    os.makedirs(save_log_dir, exist_ok=True)
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
+                        handlers=[logging.FileHandler(os.path.join(save_log_dir,
+                                                                   file_name.replace("_summary.txt",
+                                                                                     "_logfile.log"))),
+                                  logging.StreamHandler(sys.stdout)])
+
+    main()
+
+
